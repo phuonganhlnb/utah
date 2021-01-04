794332737b1f64a3da3025930f5c575bf1386aa8
minatouzuki <72920726+minatouzuki@users.noreply.github.com>
2021-01-04 21:36:40 +0530

    Update text.py

--- utah/modules/utils/text.py
+++ utah/modules/utils/text.py
@@ -1,4 +1,4 @@
-# This file is part of Texas (Telegram Bot)
+# This file is part of Utah (Telegram Bot)
 
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as
@@ -13,7 +13,7 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
-# This file is part of Texas.
+# This file is part of Utah.
 
 from typing import Union
 
