e4aaf586ad9841548ee4ecd9e4251bfadc4930ca
minatouzuki <72920726+minatouzuki@users.noreply.github.com>
2021-01-04 21:13:19 +0530

    Update locks.py

--- utah/modules/locks.py
+++ utah/modules/locks.py
@@ -1,4 +1,4 @@
-# This file is part of Texas (Telegram Bot)
+# This file is part of Utah (Telegram Bot)
 
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as
@@ -17,8 +17,8 @@ import itertools
 
 from aiogram.types.chat_permissions import ChatPermissions
 
-from texas import bot
-from texas.decorator import register
+from utah import bot
+from utah.decorator import register
 from .utils.connections import chat_connection
 from .utils.language import get_strings_dec
 
