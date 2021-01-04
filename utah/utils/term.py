7639b3c698456d6780474ce8fea22c200aeb8ec0
minatouzuki <72920726+minatouzuki@users.noreply.github.com>
2021-01-04 22:21:16 +0530

    Update term.py

--- utah/utils/term.py
+++ utah/utils/term.py
@@ -1,4 +1,4 @@
-# This file is part of Texas (Telegram Bot)
+# This file is part of Utah (Telegram Bot)
 
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as
@@ -15,7 +15,7 @@
 
 import subprocess
 
-from texas.utils.logger import log
+from utah.utils.logger import log
 
 
 def term(cmd):
